ab88ac784adc77859f9c172ae338364f302ee5bf
christianc355 <schavezent@gmail.com>
2022-10-26 17:54:29 -0600

    wifi provisioning works!

---
+++ bleWifiSetup/src/bleWifiSetup.cpp
@@ -0,0 +1,81 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#line 1 "/home/sun/Documents/SwiftletLaboratories-animated-octo-waddle/bleWifiSetup/src/bleWifiSetup.ino"
+/*
+ * Project bleWifiSetup 
+ * Description: Allow user to set custom wifi credentials on particle argon
+ * Author: Christian Chavez
+ * Date: 1666826690 UNIX
+ * SOLE PROPERTY OF CHRISTIAN CHAVEZ
+ */
+
+#include "Particle.h"
+#include "ble_wifi_setup_manager.h"
+
+// This example does not require the cloud so you can run it in manual mode or
+// normal cloud-connected mode
+void provisionCb();
+void setup();
+void loop();
+#line 14 "/home/sun/Documents/SwiftletLaboratories-animated-octo-waddle/bleWifiSetup/src/bleWifiSetup.ino"
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+
+SerialLogHandler logHandler(LOG_LEVEL_WARN, {
+    {"app", LOG_LEVEL_ALL},
+    {"system.ctrl.ble", LOG_LEVEL_ALL},
+    {"wiring.ble", LOG_LEVEL_ALL},
+});
+
+BLEWiFiSetupManager wifi_manager;
+
+typedef enum {
+    STATE_IDLE = 0,
+    STATE_PROVISIONED
+} ProvisionStates_t;
+
+ProvisionStates_t provision_state, next_provision_state;
+
+void provisionCb() {
+    provision_state = STATE_PROVISIONED;
+}
+
+void setup() {
+	provision_state      = STATE_IDLE;
+    next_provision_state = STATE_IDLE;
+    
+    wifi_manager.setup();
+    wifi_manager.setProvisionCallback(provisionCb);
+}
+
+void loop() {
+	wifi_manager.loop();
+
+    // State machine to handle WiFi provisioning states
+    switch(provision_state) {
+        case STATE_IDLE: {
+            next_provision_state = STATE_IDLE;
+            break;
+        }
+
+        case STATE_PROVISIONED: {
+            // Cycle WiFi state if needed (eg. bad password was entered and it's trying to connect)
+            if (WiFi.ready() || WiFi.connecting()) {
+                WiFi.disconnect();
+            }
+            
+            // Then connect to cloud
+            Particle.connect();
+            
+            next_provision_state = STATE_IDLE;
+            break;
+        }
+    }
+
+    if (provision_state != next_provision_state) {
+        Log.info("Provision State Transition: %u -> %u", provision_state, next_provision_state);
+        provision_state = next_provision_state;
+    }
+}
